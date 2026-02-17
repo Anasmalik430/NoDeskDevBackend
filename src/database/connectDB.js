f2722341c131a62a81c5477e2d1e45d83073ca0b
Anasmalik57 <itsanas474@gmail.com>
2026-02-17 11:19:32 +0530

    db uri exposer fixed

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb+srv://dtteamworks_db:r0iFRs4oy1KQkfHo@cluster0.0c5zjgl.mongodb.net/nodeskdeveleper?appName=Cluster0"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: 😃 ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
